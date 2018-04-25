9b64cdc705bbbbef15272ed86c2fe7288902bdd6
Alvaro Viveiros <alvarodeviveirosneto@gmail.com>
2018-04-25 11:44:32 -0300

    inputTxt

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,19 +1,26 @@
-import { Alert, StatusBar } from 'react-native'
+import { Alert, StatusBar, TextInput, View } from 'react-native'
 import React from "react";
 
 import { onSignOut } from "../services/auth";
-import {  Button, Icon } from 'react-native-elements'
+import { Button, Icon } from 'react-native-elements'
 import { Onboarding } from 'react-native-onboarding-swiper'
 
 export default ({ navigation }) => (
-    <Button
-        title="Deslogar"
-        containerViewStyle={{ marginTop: 20 }}
-        backgroundColor="white"
-        borderRadius={5}
-        textStyle={{ color: '#003c8f' }}
-        onPress={() => {
-            onSignOut().then(() => navigation.navigate("Deslogado"));
+    <View>
+        <TextInput
+            keyboardType="numeric"
+            placeholder="Número da Comanda"
+            blurOnSubmit={true}
+        />
+        <Button
+            title="Deslogar"
+            containerViewStyle={{ marginTop: 20 }}
+            backgroundColor="white"
+            borderRadius={5}
+            textStyle={{ color: '#003c8f' }}
+            onPress={() => {
+                onSignOut().then(() => navigation.navigate("Deslogado"));
         }}
     />
+    </View>
 );
