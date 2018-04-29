8489db4de1979717ab1f251447640cb064fe0333
Alvaro de Viveiros Neto <alvarodeviveirosneto@gmail.com>
2018-04-29 10:43:57 -0300

    styles and components

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export default {
+  primary: '#03AFBA',
+  background: '#242430',
+  dark_background: '#1A1A25',
+  light_background: '#303040',
+  white: '#DAE3F3',
+  red: '#E02222',
+  transparent: 'rgba(0, 0, 0, 0)'
+}
